70e53e0d378cb7c699e47348cc719414cb01f053
Roman <romagluten@gmail.com>
2019-06-12 09:44:30 +0300

    Прошелся преттиером по исходникам

--- exe/gulpfile.js
+++ exe/gulpfile.js
@@ -1,56 +1,69 @@
-const gulp = require('gulp');
-const $ = require('gulp-load-plugins')();
+const gulp = require("gulp");
+const $ = require("gulp-load-plugins")();
 
 const path = {
   src: {
-    html: './index.html',
-    scss: './src/scss/**/*.scss',
-    images: './src/img/*'
+    html: "./index.html",
+    scss: "./src/scss/**/*.scss",
+    images: "./src/img/*"
   },
   build: {
-    root: './dist',
-    css: './dist/css',
-    images: './dist/img',
-    svg: './dist/img/*.svg'
-  },
-}
+    root: "./dist",
+    css: "./dist/css",
+    images: "./dist/img",
+    svg: "./dist/img/*.svg"
+  }
+};
 
-gulp.task('styles', () => {
-  return gulp.src(path.src.scss)
+gulp.task("styles", () => {
+  return gulp
+    .src(path.src.scss)
     .pipe($.plumber())
     .pipe($.sourcemaps.init())
     .pipe($.sass())
-    .pipe($.autoprefixer({
-      grid: true
-    }))
+    .pipe(
+      $.autoprefixer({
+        grid: true
+      })
+    )
     .pipe($.cleanCss())
     .pipe($.sourcemaps.write())
-    .pipe(gulp.dest(path.build.css))
+    .pipe(gulp.dest(path.build.css));
 });
 
-gulp.task('images', () => {
-  return gulp.src(path.src.images)
+gulp.task("images", () => {
+  return gulp
+    .src(path.src.images)
     .pipe($.imagemin())
-    .pipe(gulp.dest(path.build.images))
+    .pipe(gulp.dest(path.build.images));
 });
 
-gulp.task('svg-sprite', () => {
-  return gulp.src(path.build.svg)
-    .pipe($.svgSprite({
-      mode: {
-        symbol: {
-          sprite: 'sprite.svg'
+gulp.task("svg-sprite", () => {
+  return gulp
+    .src(path.build.svg)
+    .pipe(
+      $.svgSprite({
+        mode: {
+          symbol: {
+            sprite: "sprite.svg"
+          }
         }
-      }
-    }))
-    .pipe(gulp.dest(`${path.build.images}`))
-})
-
-gulp.task('watch', () => {
-  gulp.watch(path.src.scss, gulp.series('styles'));
-  gulp.watch(path.src.images, gulp.series('images'));
+      })
+    )
+    .pipe(gulp.dest(`${path.build.images}`));
 });
 
-gulp.task('build', gulp.parallel('styles', gulp.series('images', 'svg-sprite')));
+gulp.task("watch", () => {
+  gulp.watch(path.src.scss, gulp.series("styles"));
+  gulp.watch(path.src.images, gulp.series("images"));
+});
 
-gulp.task('default', gulp.parallel('watch', 'styles', gulp.series('images', 'svg-sprite')));
+gulp.task(
+  "build",
+  gulp.parallel("styles", gulp.series("images", "svg-sprite"))
+);
+
+gulp.task(
+  "default",
+  gulp.parallel("watch", "styles", gulp.series("images", "svg-sprite"))
+);
