dd4c71440b92ce601525d21f90364bbf6048c682
Roman <romagluten@gmail.com>
2019-07-10 02:38:05 +0300

    адаптированы блоки new arrivals и banners

--- shopyy/src/js/script.js
+++ shopyy/src/js/script.js
@@ -1,13 +1,14 @@
 $(document).ready(function() {
-  $(".index-slider__list").slick({
+  $('.index-slider__list').slick({
     vertical: true,
     verticalSwiping: true,
     autoplay: true,
     arrows: false,
     dots: true,
-    appendDots: $(".index-slider__pager-container"),
+    appendDots: $('.index-slider__pager-container'),
     customPaging: function(slick, index) {
-      return `<a class="index__slider-pager-bullet" aria-label="Go to ${index + 1} slide"></a>`
+      return `<a class="index__slider-pager-bullet" aria-label="Go to ${index +
+        1} slide"></a>`;
     }
   });
 });
