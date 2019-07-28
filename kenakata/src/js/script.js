5a61ec6b36d1cdf703a10c73f669c46e2f3b2c77
Roman <romagluten@gmail.com>
2019-07-28 10:23:57 +0300

    пофикшена иконка корзины на мобильниках + пофикшено мелькание элементов с тразнишеном при загрузке

--- kenakata/src/js/script.js
+++ kenakata/src/js/script.js
@@ -1,28 +1,33 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const navBtn = document.querySelector('.page-header-nav__btn');
-  const navList = document.querySelector('.page-header-nav__list');
+document.addEventListener(
+  'DOMContentLoaded',
+  function() {
+    document.body.classList.remove('preload');
+    const navBtn = document.querySelector('.page-header-nav__btn');
+    const navList = document.querySelector('.page-header-nav__list');
 
-  navBtn.addEventListener('click', function(e) {
-    e.preventDefault();
-    navList.classList.add('active');
-  });
+    navBtn.addEventListener('click', function(e) {
+      e.preventDefault();
+      navList.classList.add('active');
+    });
 
-  window.addEventListener('click', function(e) {
-    const classList = e.target.classList;
+    window.addEventListener('click', function(e) {
+      const classList = e.target.classList;
 
-    if (
-      !navList.classList.contains('active') ||
-      classList.contains('page-header-nav__btn') ||
-      classList.contains('btn__line')
-    ) {
-      return;
-    }
+      if (
+        !navList.classList.contains('active') ||
+        classList.contains('page-header-nav__btn') ||
+        classList.contains('btn__line')
+      ) {
+        return;
+      }
 
-    if (
-      classList.contains('page-header-nav__link') ||
-      !classList.contains('page-header-nav__list')
-    ) {
-      navList.classList.remove('active');
-    }
-  });
-}, false);
+      if (
+        classList.contains('page-header-nav__link') ||
+        !classList.contains('page-header-nav__list')
+      ) {
+        navList.classList.remove('active');
+      }
+    });
+  },
+  false
+);
