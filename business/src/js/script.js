e60d2130792faed659c8002c2b070fc8a6d5a805
Roman <romagluten@gmail.com>
2019-08-29 20:17:31 +0300

    сделан "липкий" header

--- business/src/js/script.js
+++ business/src/js/script.js
@@ -1,16 +1,16 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', () => {
   // mobile menu
   const openMenuBtn = document.querySelector('.page-header__open-btn');
   const menu = document.querySelector('.page-header-nav');
   const body = document.body;
 
-  openMenuBtn.addEventListener('click', function (e) {
+  openMenuBtn.addEventListener('click', e => {
     e.preventDefault();
     menu.classList.add('active');
     body.classList.add('overlay');
   });
 
-  body.addEventListener('click', function (e) {
+  body.addEventListener('click', e => {
     const targetClassList = e.target.classList;
 
     if (body.classList.contains('overlay') &&
@@ -21,18 +21,55 @@ document.addEventListener('DOMContentLoaded', function () {
   }, true);
 
   // smooth scroll
-  menu.addEventListener('click', function (e) {
+  menu.addEventListener('click', e => {
     if (e.target.tagName !== 'A') {
       return;
     }
 
     e.preventDefault();
 
-    const blockId = e.target.getAttribute('href');
+    // highlightActiveElement(e.target, 'nav__link--active');
 
-    document.querySelector(blockId).scrollIntoView({
-      behavior: "smooth",
-      block: "start"
+    const sectionId = e.target.getAttribute('href');
+
+    document.querySelector(sectionId).scrollIntoView({
+      behavior: 'smooth',
+      block: 'nearest'
     });
-  })
+  });
+
+  // fix header on scroll
+  const headerBottom = document.querySelector('.page-header__bottom');
+
+  window.addEventListener('scroll', () => {
+    if (window.pageYOffset > 200) {
+      headerBottom.classList.add('page-header__bottom--fixed');
+    } else {
+      headerBottom.classList.remove('page-header__bottom--fixed');
+    }
+  });
+
+  // scroll spy
+  const sections = document.querySelectorAll('.js-section');
+
+  const sectionsOffset = Object.create(null);
+
+  sections.forEach(section => sectionsOffset[section.id] = section.offsetTop);
+  console.log(sectionsOffset);
+
+  window.addEventListener('scroll', () => {
+    const scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
+
+    for (let sectionId in sectionsOffset) {
+      if (sectionsOffset[sectionId] <= (scrollPosition + 500)) {
+        document.querySelector('.nav__link--active').classList.remove('nav__link--active');
+        document.querySelector(`a[href*=${sectionId}]`).classList.add('nav__link--active');
+      }
+    }
+  });
 });
+
+function highlightActiveElement(el, activeClassName) {
+  document.querySelector(`.${activeClassName}`).classList.remove(activeClassName);
+  el.classList.add(activeClassName);
+}
