086d46c3d4b1eab89136cab543eb0a3407346343
Roman <romagluten@gmail.com>
2019-09-02 21:45:40 +0300

    улучшена анимация модального окна и outline для кнопки закрытия модалки

--- wordpress-intensive/src/js/script.js
+++ wordpress-intensive/src/js/script.js
@@ -12,11 +12,19 @@ document.addEventListener('DOMContentLoaded', () => {
 
   function openModal() {
     modal.classList.add('active');
+    modal.classList.add('show');
   }
 
   function closeModal(event) {
     if (event.target === modal || event.target === closeModalBtn) {
       modal.classList.remove('active');
+
+      function handler() {
+        modal.classList.remove('show');
+        modal.removeEventListener('transitionend', handler);
+      }
+
+      modal.addEventListener('transitionend', handler);
     }
   }
 });
