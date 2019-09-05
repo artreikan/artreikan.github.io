d1e27b4a647672f0116138fbbcf2f4f592843a1d
Roman <romagluten@gmail.com>
2019-09-05 16:09:38 +0300

    сверстан каталог

--- pulsometers/src/js/script.js
+++ pulsometers/src/js/script.js
@@ -1,4 +1,5 @@
-$(document).ready(function(){
+$(document).ready(function () {
+  // slick init
   $('.carousel-items').slick({
     autoplay: true,
     prevArrow: $('.carousel__btn_prev'),
@@ -11,5 +12,18 @@ $(document).ready(function(){
         }
       }
     ]
+  });
+
+  // tabs
+  $('ul.catalog__tabs-caption').on('click', 'li:not(.catalog__tab_active)', function() {
+    $(this)
+    .addClass('catalog__tab_active')
+    .siblings()
+    .removeClass('catalog__tab_active')
+    .closest('div.container')
+    .find('div.catalog__tabs-content')
+    .removeClass('catalog__tabs-content_active')
+    .eq($(this).index())
+    .addClass('catalog__tabs-content_active');
   });
 });
